53916a86eed5b27226192720a1047d71d21216ec
Alexandra Nero <alexandra.nero@cimpress.com>
2021-08-13 21:32:23 -0500

    add ability to search by tags

---
+++ src/view/SearchSection.js
@@ -0,0 +1,65 @@
+import React, {useState} from "react";
+import { Input, Icon } from "semantic-ui-react";
+import { defaultTags } from "../edit/Tags";
+import { searchRecipe } from "../serviceCalls";
+
+function SearchSection({
+  token,
+  setErrorState,
+  setRecipes,
+  setNumberOfRecipes,
+  setIsLoading,
+  setDisablePagination,
+  setShouldRefresh,
+}) {
+  
+  const [searchField, setSearchField] = useState("");
+
+  async function submitSearch() {
+    setIsLoading(true);
+
+    if (searchField !== ""){
+      const queryParameters = defaultTags.includes(searchField) ?
+        {tags: [searchField]} :
+        {recipeName: searchField};
+      const response = await searchRecipe(queryParameters, token);
+      if (response.status !== 200) {
+        setErrorState("Error Retrieving Recipes");
+        setRecipes([]);
+        setNumberOfRecipes(1);
+      } else if (!response.data.length) {
+        setErrorState("No Matching Recipes Found");
+        setNumberOfRecipes(1);
+        setRecipes(response.data);
+      } else {
+        setErrorState("");
+        setRecipes(response.data);
+      }
+      setNumberOfRecipes(1)
+      setIsLoading(false);
+      setDisablePagination(true);
+    } else {
+      setShouldRefresh(true);
+    }
+  }
+
+  async function onInputChange(event) {
+    if (event.key === 'Enter') {
+      submitSearch();
+    }
+  }
+
+
+
+  return (
+    <Input
+      fluid
+      placeholder="Search Recipe By Name or By Tag..."
+      icon={<Icon name="search" color='orange' inverted circular link onClick={() => submitSearch()} />}
+      onChange={(event) => setSearchField(event.target.value)}
+      onKeyPress={async (event) => await onInputChange(event)}
+    />
+  );
+}
+
+export default SearchSection;
